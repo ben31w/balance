74e94fc0218cb1dfeb45093bb53423bd33da06bf
benjamin wright <benjamin.wright.20@cnu.edu>
2023-09-29 14:11:03 +0000

    update settings.py and wsgi.py so that EC2 can run the project through Apache

--- balance/settings.py
+++ balance/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = config('DEBUG', default=False, cast=bool)
 SECRET_KEY = config('SECRET_KEY')
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ec2-3-16-48-29.us-east-2.compute.amazonaws.com', '3.23.171.165']
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'staticfiles/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- balance/wsgi.py
+++ balance/wsgi.py
@@ -7,9 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/home/ubuntu/balance')
+sys.path.append('/home/ubuntu/balance/balance')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'balance.settings')
 
