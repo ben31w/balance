83ce0232c7c38510e389d3b36b10477c7ae5ba4b
benjamin wright <benjamin.wright.20@cnu.edu>
2023-10-03 17:44:54 -0400

    bugfix: edit set page now works perfectly. create a skeleton weekly view page.

--- workout_log/forms.py
+++ workout_log/forms.py
@@ -16,3 +16,11 @@ class SetForm(forms.ModelForm):
         # https://docs.djangoproject.com/en/4.2/topics/forms/modelforms/
         fields = ['date', 'exercise', 'reps', 'weight']
 
+
+class WeeklyForm(forms.Form):
+    """
+    Form on where the user can adjust the start and end dates for the Volume
+    Manager in the Weekly View
+    """
+    start_date = forms.DateField(label="Start date")
+    end_date = forms.DateField(label="End date")
--- workout_log/views.py
+++ workout_log/views.py
@@ -1,17 +1,19 @@
 """
 Views for the Workout Log app.
 """
+from datetime import date, timedelta
+
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
 from django.shortcuts import render, redirect
 
-from .forms import SetForm
-from .models import Set
+from .forms import SetForm, WeeklyForm
+from .models import Muscle, MuscleWorked, Set
 
 
 @login_required
 def index(request):
-    """Load the Workout Log home page."""
+    """Load the Workout Log home page (Daily view)."""
     sets = Set.objects.filter(logged_by=request.user)
     dates = []
     for s in sets:
@@ -54,8 +56,35 @@ def edit_set(request, set_id):
         if form.is_valid():
             form.save()
             return redirect('workout_log:index')
-    context = {'form': form}
+    context = {'form': form, 'set': set}
     return render(request, 'workout_log/edit_set.html', context)
+
+
+@login_required()
+def weekly(request):
+    """Load the Weekly View page"""
+    # TODO fix this
+    end_date = date.today()
+    start_date = end_date - timedelta(days=7)
+    if request.method == 'GET':
+        form = WeeklyForm()
+    elif request.method == 'POST':
+        form = WeeklyForm(data=request.POST)
+        if form.is_valid():
+            start_date = form.start_date
+            end_date = form.end_date
+
+    muscles = Muscle.objects.all()
+    muscles_worked = MuscleWorked.objects.all()
+    context = {
+        'form': form,
+        'start_date': start_date,
+        'end_date': end_date,
+        'muscles': muscles,
+        'muscles_worked': muscles_worked
+    }
+    return render(request, 'workout_log/weekly.html', context)
+
 
 
 def verify_user_is_owner(owner, user):
