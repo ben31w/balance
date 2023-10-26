ecc854621c109cfdb6489641ac6685f666d8439a
benjamin wright <benjamin.wright.20@cnu.edu>
2023-10-25 21:58:03 -0400

    add Nutrition Log models

--- nutrition_log/admin.py
+++ nutrition_log/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import DailyWeight, FoodItem, Recipe, Unit, Contains, LoggedFoodItem
+
+
+admin.site.register(DailyWeight)
+admin.site.register(FoodItem)
+admin.site.register(Recipe)
+admin.site.register(Unit)
+admin.site.register(Contains)
+admin.site.register(LoggedFoodItem)
--- nutrition_log/models.py
+++ nutrition_log/models.py
@@ -1,3 +1,56 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class DailyWeight(models.Model):
+    """A weight that a user can log for the day"""
+    date = models.DateField()
+    weight = models.FloatField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class FoodItem(models.Model):
+    """A food item that a user can log"""
+    # Should users be allowed to delete food items?
+    name = models.CharField(max_length=100)
+    producer = models.CharField(blank=True, max_length=25)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+
+class Recipe(models.Model):
+    """A collection of food items that a user can log"""
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+
+class Unit(models.Model):
+    """A unit that a food item can be measured in"""
+    # Should users be allowed to delete units?
+    name = models.CharField(max_length=25)
+    calsPerUnit = models.FloatField()
+    proPerUnit = models.FloatField()
+    carbsPerUnit = models.FloatField()
+    fatsPerUnit = models.FloatField()
+    food_item = models.ForeignKey(FoodItem, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    
+
+class Contains(models.Model):
+    """Link between a recipe and a food item the recipe contains"""
+    # TODO figure how to handle what happens to this class if the 
+    #  food item or unit it is linked to is deleted
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    food_item = models.ForeignKey(FoodItem, on_delete=models.RESTRICT)
+    quantity = models.FloatField()
+    unit = models.ForeignKey(Unit, on_delete=models.RESTRICT)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+
+class LoggedFoodItem(models.Model):
+    """An instance of a food item logged by a user"""
+    date = models.DateField()
+    food_item = models.ForeignKey(FoodItem, on_delete=models.RESTRICT)
+    quantity = models.FloatField()
+    unit = models.ForeignKey(Unit, on_delete=models.RESTRICT)
+    meal = models.IntegerField()  # 0 for snack, 1 for breakfast, 2 for lunch, etc.
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
