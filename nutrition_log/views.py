1406b54fc00b680b8c040dd36d1246d4df2d4e7e
benjamin wright <benjamin.wright.20@cnu.edu>
2023-11-06 17:15:39 -0500

    create a hardcoded weekly view to display days of week and weight logged for each day

--- nutrition_log/views.py
+++ nutrition_log/views.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from django.contrib.auth.decorators import login_required
 from django.core import serializers
 from django.core.exceptions import ObjectDoesNotExist
@@ -65,6 +67,7 @@ def set_weight(request):
     return render(request, 'nutrition_log/set_weight.html', context)
 
 
+@login_required
 def weekly(request):
     """Load the weekly page"""
     daily_weights = DailyWeight.objects.filter(user=request.user).order_by("date")
@@ -73,6 +76,17 @@ def weekly(request):
         return render(request, 'nutrition_log/weekly.html', context)
     
     first_entry = daily_weights[0]
-    for daily_weight in daily_weights:
-        print(daily_weight["date"])
+    first_date = first_entry.date
+    print(first_date)
+
+    start_date = date(2023, 10, 22)
+    dates = [start_date]
+    day = start_date.day
+    for i in range(1, 7):
+        # try-except if the day is past the month.
+        d = date(2023, 10, day + i)
+        dates.append(d)
+    print(dates)
+    context = {'daily_weights': daily_weights, 'dates': dates}
+    return render(request, 'nutrition_log/weekly.html', context)
 
