315d6f11608b24e9d45779f157813447bb5b6df4
benjamin wright <benjamin.wright.20@cnu.edu>
2023-11-19 17:14:54 -0500

    print all data when the workout form is submitted

--- workout_designer/views.py
+++ workout_designer/views.py
@@ -12,9 +12,39 @@ def index(request):
 def create_workout(request):
     """Loads the form for creating a workout"""
     if request.method == "POST":
+        # TODO add validation to make sure the fields are filled out.
         schedule = request.POST.get("schedule")
+
+        lower_hr_str = request.POST.get("lowerLimitHrs")
+        lower_min_str = request.POST.get("lowerLimitMin")
+        upper_hr_str = request.POST.get("upperLimitHrs")
+        upper_min_str = request.POST.get("upperLimitMin")
+        lower_limit = get_minutes(lower_hr_str, lower_min_str)
+        upper_limit = get_minutes(upper_hr_str, upper_min_str)
+
+        goal = request.POST.get("goal")
+
+        split = request.POST.get("split")
+
         print(schedule)
+        print(f"Lower limit: {lower_limit}")
+        print(f"Upper limit: {upper_limit}")
+        print(goal)
+        print(split)
     else:
         return render(request, 'workout_designer/create_workout.html')
     return render(request, 'workout_designer/create_workout.html')
 
+
+def get_minutes(hrs, min):
+    """Given strings for hrs and min, get the total min as an int"""
+    total = 0
+    try:
+        total += int(hrs) * 60
+    except ValueError:
+        pass
+    try:
+        total += int(min)
+    except ValueError:
+        pass
+    return total
