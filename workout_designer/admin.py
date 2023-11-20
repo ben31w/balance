b0b0d88c535beb6fb4fc3bc62dde3afef1cfd09d
benjamin wright <benjamin.wright.20@cnu.edu>
2023-11-19 19:43:55 -0500

    register WD models

--- workout_designer/admin.py
+++ workout_designer/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Routine, Day, PlannedSets
+
 # Register your models here.
+admin.site.register(Routine)
+admin.site.register(Day)
+admin.site.register(PlannedSets)
