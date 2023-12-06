e3b1a68d58cb14ebb1e240f8c2eaec8556cb5e58
benjamin wright <benjamin.wright.20@cnu.edu>
2023-12-06 10:37:22 -0500

    fix URLs by adding / to end

--- nutrition_log/urls.py
+++ nutrition_log/urls.py
@@ -9,14 +9,14 @@ app_name = 'nutrition_log'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('daily', views.daily, name='daily'),
+    path('daily/', views.daily, name='daily'),
     path('set_weight/<int:year>/<int:month>/<int:day>/', views.set_weight, name='set_weight'),
     path('log_food_item/<int:year>/<int:month>/<int:day>/', views.log_food_item, name='log_food_item'),
     path('edit_logged_food_item/<int:lfi_id>/', views.edit_logged_food_item, name='edit_logged_food_item'),
     path('delete_logged_food_item/<int:lfi_id>/', views.delete_logged_food_item, name='delete_logged_food_item'),
-    path('weekly', views.weekly, name='weekly'),
-    path('charts', views.charts, name='charts'),
-    path('charts/bodyweight', views.create_weight_chart, name='create_weight_chart'),
-    path('charts/calories', views.create_calories_chart, name='create_calories_chart'),
-    path('set_target_calories', views.set_target_calories, name='set_target_calories'),
+    path('weekly/', views.weekly, name='weekly'),
+    path('charts/', views.charts, name='charts'),
+    path('charts/bodyweight/', views.create_weight_chart, name='create_weight_chart'),
+    path('charts/calories/', views.create_calories_chart, name='create_calories_chart'),
+    path('set_target_calories/', views.set_target_calories, name='set_target_calories'),
 ]
